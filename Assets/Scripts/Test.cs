0692ded5c850e5866cc73a1113651037767b1256
heegwon <gnrms0507@gmail.com>
2025-10-24 13:58:41 +0900

    test: 충돌테스트
    test

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -7,6 +7,7 @@ public class Test : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("테스트입니다")
+        Debug.Log("테스트입니다");
+        Debug.Log("안녕하세요 여러분 반갑습니다");
     }
 }
