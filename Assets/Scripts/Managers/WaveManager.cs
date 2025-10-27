de40d3e402e832f7742535f5e73966d6bd24494c
Manseong Han <akstjd31@naver.com>
2025-10-28 02:02:06 +0900

    refactor : 클래스 위치 변경 및 데이터 직렬화
    <type>: 테스트를 위한 심플 이동 코드 구현
    
    <body>
    - 관리자 내부 웨이브 클래스 선언
    - 웨이브에 필요한 변수들 정의(현 웨이브 수치, 생성된 적, 웨이브 시작 유무 등)
    
    <footer>

--- Assets/Scripts/Managers/WaveManager.cs
+++ Assets/Scripts/Managers/WaveManager.cs
@@ -4,21 +4,46 @@ using UnityEngine;
 
 public class WaveManager : MonoBehaviour
 {
-    List<Wave> waves = new List<Wave>();
-    int currentWaveIndex;
-    public event Action onWaveStarted;
-    public event Action onWaveEnded;
+    [Serializable]
+    public class Wave
+    {
+        public int waveNum;            // 웨이브 번호
+        public int waveEnemyCount;     // 웨이브 당 생성될 적 수
+        public float spawnInterval;    // 스폰 주기
+        public List<Enemy> enemies;    // 해당 웨이브에 소환될 적 리스트
+    }
+
+    [SerializeField] List<Wave> waves = new List<Wave>();
+    public event Action onWaveStarted;  // 웨이브 시작 시 이벤트
+    public event Action onWaveEnded;    // 웨이브 종료 시 이벤트
+    private int currentWaveNum;         // 현재 (진행한, 진행중인) 웨이브
+    private int spawnedCount;           // 생성된 적 카운트
+    private float spawnTimer;
+    private bool isWaveRunning = false;
 
     // 임시 데이터
     private void Start()
     {
-        Wave wave = new Wave();
+        currentWaveNum = 1;
+    }
 
-        wave.waveNum = 1;
-        wave.waveEnemyCount = 10;
-        wave.spawnInterval = 1f;
-        wave.enemies = null;
+    private void StartWave()
+    {
+        if (currentWaveNum >= waves.Count)
+            return;
 
-        currentWaveIndex = wave.waveNum;
+        isWaveRunning = true;
+        spawnedCount = 0;
+
+        onWaveStarted?.Invoke();
+
+        Debug.Log($"Wave {currentWaveNum} 시작!");
+    }
+
+    private void RunWave()
+    {
+        Wave wave = waves[currentWaveNum];
+
+        
     }
 }
