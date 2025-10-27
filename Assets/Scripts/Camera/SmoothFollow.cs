8e4432bbf2535becb66c1e18865258f4b5a1580a
Manseong Han <akstjd31@naver.com>
2025-10-28 01:11:43 +0900

    test : 간단한 플레이어 이동, 카메라 이동
    <type>: 테스트를 위한 심플 이동 코드 구현
    
    <body>
    - 플레이어 이동 (적은 아직)
    - 카메라가 플레이어를 따라다님
    
    <footer>

---
+++ Assets/Scripts/Camera/SmoothFollow.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmoothFollow : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+    [SerializeField] private float smoothSpeed;
+    [SerializeField] Vector3 offset;
+
+    private void LateUpdate()
+    {
+        if (target == null)
+            return;
+
+        Vector3 desiredPosition = target.position + offset;
+
+        Vector3 smoothedPosition = Vector3.Lerp(this.transform.position, desiredPosition, smoothSpeed * Time.deltaTime);
+
+        this.transform.position = smoothedPosition;
+        
+    }
+}
